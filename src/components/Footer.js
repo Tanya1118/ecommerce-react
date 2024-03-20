468979312a6990949846c39f2aad2760546237bd
Tanya1118 <128591833+Tanya1118@users.noreply.github.com>
2024-03-20 16:24:05 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className='bg-black py-12'>
+    <div className="container mx-auto">
+      <p className='text-white text-center'>Copyright &copy; Ecommerce Shop 2024. All rights are reserved.</p>
+    </div>
+  </footer>;
+};
+
+export default Footer;
